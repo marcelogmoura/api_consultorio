2e825c7bf4ae3c53643fc8a755f28d6fc773cd4a
marcelogmoura <mgmoura@gmail.com>
2023-08-01 23:03:02 -0300

    paciente controller implementado

--- src/main/java/com/mgmoura/controllers/PacientesController.java
+++ src/main/java/com/mgmoura/controllers/PacientesController.java
@@ -1,6 +1,10 @@
 package com.mgmoura.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.mgmoura.dtos.PacientesPostRequestDto;
 import com.mgmoura.dtos.PacientesPutRequestDto;
+import com.mgmoura.entities.Paciente;
 import com.mgmoura.repositories.PacienteRepository;
 
 @RestController
@@ -22,27 +27,100 @@ public class PacientesController {
 	private PacienteRepository pacienteRepository;
 	
 	@PostMapping
-	public void post(@RequestBody PacientesPostRequestDto dto) {
+	public ResponseEntity<String> post(@RequestBody PacientesPostRequestDto dto) {
 		
+		try {
+			
+			Paciente paciente = new Paciente();
+			paciente.setNome(dto.getNome());
+			paciente.setDataNascimento(dto.getDataNascimento());
+			paciente.setTelefone(dto.getTelefone());
+			
+			pacienteRepository.save(paciente);
+			
+			return ResponseEntity.status(201).body("Paciente cadastrado com sucesso.");
+			
+		}catch (Exception e) {
+			return ResponseEntity.status(500).body("Erro ao cadastrar paciente" + e.getMessage());
+		}
 	}
 	
 	@PutMapping
-	public void put(@RequestBody PacientesPutRequestDto dto) {
+	public ResponseEntity<String> put(@RequestBody PacientesPutRequestDto dto) {
+		try {
+			Optional<Paciente> paciente = pacienteRepository.findById(dto.getIdPaciente());
+			
+			if(paciente.isEmpty()) {
+				return ResponseEntity.status(400).body("Paciente não encontrado");
+				
+			}else {
+				
+				Paciente item = paciente.get();
+				item.setNome(dto.getNome());
+				item.setTelefone(dto.getTelefone());
+				item.setDataNascimento(dto.getDataNascimento());
+				
+				pacienteRepository.save(item);
+				
+				return ResponseEntity.status(200).body("Dados atualizados com sucesso.");
+			}
+			
+		}catch (Exception e) {
+			return ResponseEntity.status(500).body("Falha ao atualizar: " + e.getMessage());
+		}
+		
 		
 	}
 	
 	@DeleteMapping("{idPaciente}")
-	public void delete(@PathVariable("idPaciente") Integer idPaciente) {
+	public ResponseEntity<String> delete(@PathVariable("idPaciente") Integer idPaciente) {
+		
+		try {
+			
+			Optional<Paciente> paciente = pacienteRepository.findById(idPaciente);
+			
+			if(paciente.isEmpty()) {
+				return ResponseEntity.status(400).body("Paciente não encontrado.");
+			}else {
+				pacienteRepository.delete(paciente.get());
+				return ResponseEntity.status(200).body("Paciente excluído com sucesso");
+			}
+			
+		}catch (Exception e) {
+			return ResponseEntity.status(500)
+					.body("Falha ao excluir: " 	+ e.getMessage());
+		}
 		
 	}
 	
 	@GetMapping
-	public void getAll() {
+	public ResponseEntity<List<Paciente>> getAll() {
 		
+		try {
+			List<Paciente> pacientes = pacienteRepository.findAll();
+			return ResponseEntity.status(200).body(pacientes);
+			
+		}catch (Exception e) {
+			return ResponseEntity.status(500).body(null);
+		}
 	}
 	
 	@GetMapping("{idPaciente}")
-	public void getById(@PathVariable("idPaciente") Integer idPaciente) {
+	public ResponseEntity<Paciente> getById(@PathVariable("idPaciente") Integer idPaciente) {
+		
+		try {
+			Optional<Paciente> paciente = pacienteRepository.findById(idPaciente);
+			
+			if(paciente.isPresent()) {
+				return ResponseEntity.status(200).body(paciente.get());
+				
+			}else {
+				return ResponseEntity.status(204).body(null);
+			}
+			
+		}catch (Exception e) {
+			return ResponseEntity.status(500).body(null);
+		}
 		
 	}
 
