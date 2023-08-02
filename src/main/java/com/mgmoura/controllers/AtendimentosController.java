20d34cc551af07ea9b315a29f436f2a37c44505f
marcelogmoura <mgmoura@gmail.com>
2023-08-02 17:58:59 -0300

    atendimento controller

--- src/main/java/com/mgmoura/controllers/AtendimentosController.java
+++ src/main/java/com/mgmoura/controllers/AtendimentosController.java
@@ -1,6 +1,12 @@
 package com.mgmoura.controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mgmoura.dtos.AtendimentosPostRequestDto;
+import com.mgmoura.entities.Atendimento;
+import com.mgmoura.entities.Paciente;
 import com.mgmoura.repositories.AtendimentoRepository;
+import com.mgmoura.repositories.PacienteRepository;
 
 import jakarta.validation.Valid;
 
@@ -21,27 +30,66 @@ public class AtendimentosController {
 	
 	@Autowired
 	private AtendimentoRepository atendimentoRepository;
+
+	@Autowired
+	private PacienteRepository pacienteRepository;
 	
 	@PostMapping
-	public void post(@RequestBody @Valid AtendimentosPostRequestDto dto) {
+	public ResponseEntity<String> post(@RequestBody @Valid AtendimentosPostRequestDto dto) {
 		
+		try {
+			Optional<Paciente> paciente = pacienteRepository.findById(dto.getIdPaciente());
+			
+			if(paciente.isEmpty()) {
+				return ResponseEntity.status(400).body("Paciente não localizado.");
+				
+			}else {
+				Atendimento atendimento = new Atendimento();
+				atendimento.setPaciente(paciente.get());
+				atendimento.setDataAtendimento(dto.getDataAtendimento());
+				atendimento.setObservacoes(dto.getObservacoes());
+				
+				atendimentoRepository.save(atendimento);
+				
+				return ResponseEntity.status(201).body("Atendimento cadastrado com sucesso.");
+				
+			}
+			
+		}catch (Exception e) {
+			return ResponseEntity.status(500).body("Falha ao cadastrar: " +e.getMessage());
+			
+		}
 	}
 	
 	@PutMapping  // @Valid nao esquecer
 	public void put() {
+		
 		
 	}
 	
 	@DeleteMapping
 	public void delete() {
 		
+		
 	}
 	
 	@GetMapping("{dataInicio}/{dataFim}")
-	public void getAll(
+	public ResponseEntity<List<Atendimento>> getAll(
 			@PathVariable() String dataInicio,
-			@PathVariable() String dataFim
-			) {
+			@PathVariable() String dataFim) {
+		
+		try {
+			
+			Date dataMin = new SimpleDateFormat("yyyy-MM-dd").parse(dataInicio);
+			Date dataMax = new SimpleDateFormat("yyyy-MM-dd").parse(dataFim);
+			
+			List<Atendimento> atendimentos = atendimentoRepository.findByDatas(dataMin, dataMax);
+			
+			return ResponseEntity.status(200).body(atendimentos);
+			
+		}catch (Exception e) {
+			return ResponseEntity.status(500).body(null);
+		}
 		
 	}
 
--- src/main/java/com/mgmoura/repositories/AtendimentoRepository.java
+++ src/main/java/com/mgmoura/repositories/AtendimentoRepository.java
@@ -1,11 +1,26 @@
 package com.mgmoura.repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.mgmoura.entities.Atendimento;
 
 @Repository
 public interface AtendimentoRepository extends JpaRepository<Atendimento, Integer>{
+
+	@Query("SELECT a FROM Atendimento a " +
+		       "JOIN FETCH a.paciente p " +
+		       "WHERE a.dataAtendimento >= :dataInicial " +
+		       "AND a.dataAtendimento <= :dataFinal " +
+		       "ORDER BY a.dataAtendimento DESC")
+	
+	List<Atendimento> findByDatas(
+			@Param("dataInicial") Date dataMin, 
+			@Param("dataFinal") Date dataMax);
 
 }
